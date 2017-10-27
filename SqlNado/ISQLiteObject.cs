8d330294747a1ca88b77560ac371bf5d4d18ca1d
smourier <simon_mourier@hotmail.com>
2017-10-27 19:44:05 +0200

    Work in progress

--- SqlNado/ISQLiteObject.cs
+++ SqlNado/ISQLiteObject.cs
@@ -3,7 +3,6 @@ namespace SqlNado
 {
     public interface ISQLiteObject
     {
-        object[] PrimaryKey { get; }
         bool OnLoadAction(SQLiteObjectAction action, SQLiteStatement statement, SQLiteLoadOptions options);
         bool OnSaveAction(SQLiteObjectAction action, SQLiteSaveOptions options);
     }
