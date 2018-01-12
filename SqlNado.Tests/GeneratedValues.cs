a24f5474b69d25b0fbeabd8a16edb3184d778774
Gérald Barré <meziantou@live.fr>
2018-01-11 23:39:56 -0500

    Fix cannot insert row with no insert parameter

---
+++ SqlNado.Tests/GeneratedValues.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlNado.Tests
+{
+    [TestClass]
+    public class GeneratedValues
+    {
+        [TestMethod]
+        public void AutomaticColumn_AutoIncrement()
+        {
+            using (var db = new SQLiteDatabase(":memory:"))
+            {
+                db.Save(new AutoIncrement());
+                db.Save(new AutoIncrement());
+                db.Save(new AutoIncrement());
+
+                var customers = db.LoadAll<AutoIncrement>();
+                CollectionAssert.AreEquivalent(new[] { 1, 2, 3 }, customers.Select(c => c.Id).ToList());
+            }
+        }
+
+        internal class AutoIncrement
+        {
+            [SQLiteColumn(IsPrimaryKey = true, AutoIncrements = true)]
+            public int Id { get; set; }
+        }
+    }
+}
