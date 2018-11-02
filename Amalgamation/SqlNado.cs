eea91d7607f98148baf1bf94a3f4b0a4b21df5e0
Simon Mourier <simon_mourier@hotmail.com>
2018-11-02 20:19:20 +0100

    Escaped create index column names.

--- Amalgamation/SqlNado.cs
+++ Amalgamation/SqlNado.cs
@@ -3542,12 +3542,14 @@ namespace SqlNado
         }
 
         public string Name { get; }
+        [Browsable(false)]
+        public string EscapedName => SQLiteStatement.EscapeName(Name);
         public virtual string CollationName { get; set; }
         public virtual SQLiteDirection? Direction { get; set; }
 
         public virtual string GetCreateSql()
         {
-            string s = Name;
+            string s = EscapedName;
             if (!string.IsNullOrWhiteSpace(CollationName))
             {
                 s += " COLLATE " + CollationName;
--- SqlNado/SQLiteIndexedColumn.cs
+++ SqlNado/SQLiteIndexedColumn.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace SqlNado
 {
@@ -13,12 +14,14 @@ namespace SqlNado
         }
 
         public string Name { get; }
+        [Browsable(false)]
+        public string EscapedName => SQLiteStatement.EscapeName(Name);
         public virtual string CollationName { get; set; }
         public virtual SQLiteDirection? Direction { get; set; }
 
         public virtual string GetCreateSql()
         {
-            string s = Name;
+            string s = EscapedName;
             if (!string.IsNullOrWhiteSpace(CollationName))
             {
                 s += " COLLATE " + CollationName;
